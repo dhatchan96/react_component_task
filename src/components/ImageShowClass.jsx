4c822d54faa1647972a2904edb9945d744884ee0
dhatchan96 <dhatchan0@gmail.com>
2024-09-09 10:46:26 +0530

    Added image path

--- src/components/ImageShowClass.jsx
+++ src/components/ImageShowClass.jsx
@@ -7,7 +7,7 @@ const ImageShowClass = () => {
 
                 {
                     imageShowClass.map((image) => (
-                        <div className='row g-0' key={imageShowClass.Title}>
+                        <div className='row g-0' key={image.Index}>
                             <div className={`col-lg-6 ${image.Index % 2 == 0? 'order-lg-2':'' } text-white showcase-img`} style={{ backgroundImage: `url(${image.Image})`}}></div>
                             <div className="${image.Index % 2 == 0} ? col-lg-6 order-lg-1 my-auto showcase-text : col-lg-6 my-auto showcase-text">
                             <h2>{image.Title}</h2>
